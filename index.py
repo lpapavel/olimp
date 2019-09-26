637af9476a6c1aa7e710558ec36aba84058bb13e
Administrator - default user with sudo previlages <admin@localhost.localdomain>
2019-09-26 17:55:40 +0000

    Middle coordinats !

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from tkinter import * #importing all from tkinter
+from tkinter import *
 
 def get_canv_params():
     print("========== - DRAWING CANVAS - ==========")
@@ -26,31 +26,57 @@ def create_field(canvas, c_height, c_width):
     return height, width
 
 def create_vertical_lines(canvas, f_height, f_width):
-    step = 0
-    while step < f_width:
-        canvas.create_line(step, 0, step, f_height)
-        step = step + 10
+    # vertical
+    vertical_mid_list = []
+    count = 0
+    step = 10
+    n = 0
+    while count < f_width:
+        canvas.create_line(count, 0, count, f_height)
+        count = count + step
+        n = n + 1
+        current_mid = (step * n) - (step / 2)
+        vertical_mid_list.append(current_mid)
+    return vertical_mid_list
+
 
 def create_horizontal_lines(canvas, f_height, f_width):
-    step = 0
-    while step < f_height:
-        canvas.create_line(0, step, f_width, step)
-        step = step + 10
+    # horisontal
+    horisontal_mid_list = []
+    step = 10
+    n = 0
+    count = 0
+    while count < f_height:
+        canvas.create_line(0, count, f_width, count)
+        count = count + step
+        n = n + 1
+        current_mid = (step * n) - (step / 2)
+        horisontal_mid_list.append(current_mid)
+    return horisontal_mid_list
+
 
 def create_sectors(canvas, f_height, f_width):
 
-    create_vertical_lines(canvas, f_height, f_width)
-    create_horizontal_lines(canvas, f_height, f_width)
+    vertical_mid_list = []
+    horizontal_mid_list = []
+
+    vertical_mid_list = create_vertical_lines(canvas, f_height, f_width)
+    horizontal_mid_list = create_horizontal_lines(canvas, f_height, f_width)
+
+    return vertical_mid_list, horizontal_mid_list
+
 
 tkinter = Tk()
 
 (canv_height, canv_width) = get_canv_params()
-
-canv = create_canvas(canv_height, canv_width, tkinter)
+canv = create_canvas(canv_height, canv_width, tkinter) # Drawing canvas
 canv.pack()
 
-(field_height, field_width) = create_field(canv, canv_height, canv_width)
+(field_height, field_width) = create_field(canv, canv_height, canv_width) # Drawing field
 
-create_sectors(canv, field_height, field_width)
+(vertical_mid_list, horizontal_mid_list) = create_sectors(canv, field_height, field_width) # Drawing sectors (step = 10px default) !!!!! -- > mid_lists - list with mid coords
+
+
+
 
 tkinter.mainloop()
