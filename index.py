cf459c26e3329e4ff4aebe6c9e907c6b5b122a6d
Administrator - default user with sudo previlages <admin@localhost.localdomain>
2019-09-29 09:09:50 +0000

    Traectory -not optimal

--- index.py
+++ index.py
@@ -90,10 +90,35 @@ def print_dots(vertical_mid_list, horizontal_mid_list, canvas, width, height):
     count = 0
     while count < vertical_mid_list.__len__():
         while element < horizontal_mid_list.__len__():
-            canvas.create_line(vertical_mid_list[count], horizontal_mid_list[element], vertical_mid_list[count] + 1, horizontal_mid_list[element])
+            canvas.create_line(vertical_mid_list[count], horizontal_mid_list[element], vertical_mid_list[count], horizontal_mid_list[element] + 1)
             element = element + 1
         count = count + 1
         element = 0
+
+def print_way(canvas, horizontal_mid_list, vertical_mid_list):
+    count = 0
+    row = 0
+    count_max = vertical_mid_list.__len__()
+    row_max = horizontal_mid_list.__len__()
+    while count < vertical_mid_list.__len__() - 1:
+        while row < horizontal_mid_list.__len__():
+            canvas.create_line(horizontal_mid_list[row], vertical_mid_list[count], horizontal_mid_list[row - 1],
+                               vertical_mid_list[count], arrow=FIRST)
+            row = row + 1
+
+        canvas.create_line(horizontal_mid_list[row - 1], vertical_mid_list[count], horizontal_mid_list[0],
+                           vertical_mid_list[count + 1])
+        row = 0
+        count = count + 1
+
+    while row < horizontal_mid_list.__len__() - 1:
+        canvas.create_line(horizontal_mid_list[row], vertical_mid_list[count_max - 1], horizontal_mid_list[row + 1],
+                           vertical_mid_list[count_max - 1], arrow=LAST)
+        row = row + 1
+
+    # go home
+    canvas.create_line(horizontal_mid_list[row_max - 1], vertical_mid_list[count_max - 1], horizontal_mid_list[0], vertical_mid_list[0])
+
 
 
 tkinter = Tk()
@@ -106,7 +131,8 @@ canv.pack()
 
 (vertical_mid_list, horizontal_mid_list) = create_sectors(canv, field_height, field_width) # Drawing sectors (step = 10px default) !!!!! -- > mid_lists - list with mid coords
 
-print_dots(vertical_mid_list, horizontal_mid_list, canv, field_width, field_height)
+#print_dots(vertical_mid_list, horizontal_mid_list, canv, field_width, field_height)
 
+print_way(canv, vertical_mid_list, horizontal_mid_list)
 
 tkinter.mainloop()
